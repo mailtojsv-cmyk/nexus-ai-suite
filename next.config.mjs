17734cd20e499942bcb699e64e250bd4e9da06dd
mailtojsv-cmyk <mailtojsv@gmail.com>
2026-03-22 12:54:43 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['api-inference.huggingface.co'],
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
